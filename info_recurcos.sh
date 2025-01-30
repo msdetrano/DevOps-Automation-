3616b0c4dce501a5f6eab1e7fb2e13384faef6d9
Marcos Detrano <msdetrano@bol.com.br>
2025-01-30 15:29:36 -0300

    Add files via upload

---
+++ info_recurcos.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "=== INFORMAÇÕES DO SERVIDOR ==="
+echo "CPU Load: $(uptime | awk -F 'load average:' '{ print $2 }')"
+echo "Memória Livre: $(free -h | awk '/Mem/{print $7}')"
+echo "Espaço em Disco: $(df -h / | awk 'NR==2 {print $4}')"
